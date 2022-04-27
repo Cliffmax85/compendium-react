fd529f448ff260486add95cb7a98a5bff7fc47d8
cliffmax85 <cliffmax85@gmail.com>
2022-04-27 16:36:37 -0700

    changed to useeffect to hit api

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import PokemonList from "./views/Pokemon/PokemonList";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <PokemonList />;
 }
--- src/views/Pokemon/PokemonList.jsx
+++ src/views/Pokemon/PokemonList.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 
 export default function PokemonList() {
-    const [pokemen, setPokemen] = useState([]);
+    const [pokemon, setPokemon] = useState([]);
 
-    async function fetchPokemon(e) {
-        e.preventDefault();
-        const res = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+  useEffect(() => {
+      const fetchPokemon = async () => {
+          const res = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+          const { data } = res.json();
+          console.log(data);
+          const pokemonData = data.map((pokes) => ({
+              img: pokes.url_image,
+              name: pokes.pokemon,
+              type: pokes.type,
+          })),
+          setPokemon(pokemonData);
+      };
+      fetchPokemon();
 
-        const json = await res.json();
-        console.log(json);
-    }
+  }, []);
+
+  return <div>Pokemon of the World</div>;
 }
